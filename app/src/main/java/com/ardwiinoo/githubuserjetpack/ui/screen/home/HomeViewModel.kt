b21f56a6a092ce1750619c7215bd5252d7f8504f
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-14 23:02:40 +0700

    modif: change to StateFlow

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/ui/screen/home/HomeViewModel.kt
@@ -1,21 +1,22 @@
 package com.ardwiinoo.githubuserjetpack.ui.screen.home
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.ardwiinoo.githubuserjetpack.data.remote.response.GithubResponse
 import com.ardwiinoo.githubuserjetpack.data.repository.ApiRepository
 import com.ardwiinoo.githubuserjetpack.utils.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val repository: ApiRepository
-): ViewModel() {
+) : ViewModel() {
 
-    private val _userList = mutableStateOf<Result<GithubResponse>>(Result.Loading)
-    val userList: State<Result<GithubResponse>> = _userList
+    private val _userList = MutableStateFlow<Result<GithubResponse>>(Result.Loading)
+    val userList: StateFlow<Result<GithubResponse>> = _userList.asStateFlow()
 
     suspend fun searchUsers(username: String) {
         _userList.value = Result.Loading
