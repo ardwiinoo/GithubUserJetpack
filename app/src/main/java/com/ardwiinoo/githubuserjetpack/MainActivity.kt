6ca7c93d21a2b0925da392538e5cf5b34aa1b482
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-14 22:57:46 +0700

    modif: display HomeScreen for test

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/MainActivity.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.ardwiinoo.githubuserjetpack.ui.screen.home.HomeScreen
 import com.ardwiinoo.githubuserjetpack.ui.theme.GithubUserJetpackTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GithubUserApp()
+                    HomeScreen()
                 }
             }
         }
