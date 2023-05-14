c4065282a8013f85d562a4502b62e4b6a6938213
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-14 11:04:18 +0700

    modif: make repository for request to api

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/data/repository/ApiRepository.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/data/repository/ApiRepository.kt
@@ -1,4 +1,30 @@
 package com.ardwiinoo.githubuserjetpack.data.repository
 
-class ApiRepository {
+import com.ardwiinoo.githubuserjetpack.data.remote.apiService.ApiService
+import com.ardwiinoo.githubuserjetpack.data.remote.response.GithubResponse
+import javax.inject.Inject
+import com.ardwiinoo.githubuserjetpack.utils.Result
+
+class ApiRepository @Inject constructor(
+    private val apiService: ApiService
+) {
+
+    suspend fun searchUsers(username: String): Result<GithubResponse> {
+
+        val response = apiService.getUsers(username)
+        val result = response.body()
+
+        return try {
+            if(response.isSuccessful && result != null) {
+                Result.Success(result)
+            } else {
+                val errorBody = response.errorBody()
+                val errorMessage = errorBody?.toString() ?: "Unknown error"
+                Result.Error(errorMessage)
+            }
+        } catch (e: Exception) {
+            Result.Error(e.message ?: "Unknown error")
+        }
+    }
+
 }
