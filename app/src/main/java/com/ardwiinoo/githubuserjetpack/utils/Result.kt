bfd73734bf3e1cf0e60325ad0984f0839390d3f3
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-14 11:06:08 +0700

    modif: make resource class for handling response

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/utils/Result.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/utils/Result.kt
@@ -1,4 +1,13 @@
 package com.ardwiinoo.githubuserjetpack.utils
 
-class Result {
+sealed class Result<out T> {
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val message: String, val exception: Exception? = null) : Result<Nothing>()
+    object Loading : Result<Nothing>()
+
+    companion object {
+        fun <T> success(data: T) = Success(data)
+        fun error(message: String, exception: Exception? = null) = Error(message, exception)
+        fun loading() = Loading
+    }
 }
