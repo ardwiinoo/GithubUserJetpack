12172434df8923a553971459036f9357d0279fb8
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-14 11:05:01 +0700

    modif: try setup navigation

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/navigation/Navigation.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/navigation/Navigation.kt
@@ -1,4 +1,11 @@
 package com.ardwiinoo.githubuserjetpack.navigation
 
-class Navigation {
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class Screen(val route: String, val icon: ImageVector, val title: String) {
+    object Home : Screen("home", Icons.Filled.Home, "Home")
+    object Favorites : Screen("favorites", Icons.Filled.Favorite, "Favorites")
 }
