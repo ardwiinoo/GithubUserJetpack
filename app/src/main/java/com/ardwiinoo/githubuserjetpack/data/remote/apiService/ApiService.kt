7fde183b0a74dadbccf2309fafb3991f961e9f2e
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-14 11:03:50 +0700

    modif: make apiService request

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/data/remote/apiService/ApiService.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/data/remote/apiService/ApiService.kt
@@ -1,4 +1,34 @@
 package com.ardwiinoo.githubuserjetpack.data.remote.apiService
 
-class ApiService {
+import com.ardwiinoo.githubuserjetpack.data.remote.response.GithubDetailResponse
+import com.ardwiinoo.githubuserjetpack.data.remote.response.GithubFollowerResponse
+import com.ardwiinoo.githubuserjetpack.data.remote.response.GithubFollowingResponse
+import com.ardwiinoo.githubuserjetpack.data.remote.response.GithubResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("search/users")
+    suspend fun getUsers(
+        @Query("q") query: String,
+    ): Response<GithubResponse>
+
+    @GET("users/{username}")
+    suspend fun getDetailUser(
+        @Path("username") username: String,
+    ): GithubDetailResponse
+
+    @GET("users/{username}/followers")
+    suspend fun getUserFollowers(
+        @Path("username") username: String,
+    ): GithubFollowerResponse
+
+    @GET("users/{username}/following")
+    suspend fun getUserFollowings(
+        @Path("username") username: String,
+    ): GithubFollowingResponse
+
 }
