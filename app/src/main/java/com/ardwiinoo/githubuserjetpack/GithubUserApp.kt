3a6d30bead5c7ef27e65a1e5401f56fbca2cfbfd
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-15 23:28:34 +0700

    modif: hilt entry point

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/GithubUserApp.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/GithubUserApp.kt
@@ -1,7 +1,15 @@
 package com.ardwiinoo.githubuserjetpack
 
 import android.app.Application
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ardwiinoo.githubuserjetpack.navigation.NavigationItem
+import com.ardwiinoo.githubuserjetpack.navigation.Screen
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GithubUserApp : Application()
+
