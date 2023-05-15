d3828f91acda2fd0f17277ab3c0dde63a6e769fa
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-15 23:34:19 +0700

    modif: make Favorite Screen

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/ui/screen/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/ui/screen/favorite/FavoriteScreen.kt
@@ -1,2 +1,11 @@
 package com.ardwiinoo.githubuserjetpack.ui.screen.favorite
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FavoriteScreen(
+    modifier: Modifier = Modifier,
+) {
+
+}
