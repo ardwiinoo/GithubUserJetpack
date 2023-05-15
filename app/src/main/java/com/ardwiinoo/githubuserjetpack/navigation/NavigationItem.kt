e197e1ea501d99ae9d7378e4a010ebedf516e3fc
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-15 23:29:53 +0700

    modif: make navigation component

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/navigation/NavigationItem.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/navigation/NavigationItem.kt
@@ -1,4 +1,9 @@
 package com.ardwiinoo.githubuserjetpack.navigation
 
-class NavigationItem {
-}
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavigationItem(
+    val title: String,
+    val icon: ImageVector,
+    val screen: Screen
+)
