34c62f684eb8e340a6376d2924ddc2d83fe85648
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-18 10:23:11 +0700

    modif: create room database

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/data/local/database/AppDatabase.kt
@@ -1,4 +1,16 @@
 package com.ardwiinoo.githubuserjetpack.data.local.database
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.ardwiinoo.githubuserjetpack.data.local.dao.FavoriteDao
+import com.ardwiinoo.githubuserjetpack.data.local.entity.Favorite
+
+@Database(
+    entities = [Favorite::class],
+    version = 2,
+    exportSchema = false
+)
+
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun favoriteDao(): FavoriteDao
 }
