e1281ee7eab95e55f63fa8a18cada0e3fcfb8388
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-18 10:25:15 +0700

    modif: make method to get data from local db

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/data/repository/LocalRepository.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/data/repository/LocalRepository.kt
@@ -1,4 +1,25 @@
 package com.ardwiinoo.githubuserjetpack.data.repository
 
-class LocalRepository {
+import com.ardwiinoo.githubuserjetpack.data.local.database.AppDatabase
+import com.ardwiinoo.githubuserjetpack.data.local.entity.Favorite
+import javax.inject.Inject
+
+class LocalRepository @Inject constructor(
+    private val database: AppDatabase
+) {
+    suspend fun insertFavorite(favorite: Favorite) {
+        database.favoriteDao().insertUser(favorite)
+    }
+
+    suspend fun deleteFavorite(username: String) {
+        database.favoriteDao().deleteFavorite(username)
+    }
+
+    suspend fun isFavoriteUser(username: String): Boolean {
+        return database.favoriteDao().isFavoriteUser(username)
+    }
+
+    suspend fun getAllFavorites(): List<Favorite> {
+        return database.favoriteDao().getAllFavorites()
+    }
 }
