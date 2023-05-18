927ca6ac3cfea13ad69903f19d04dd98632f9dc7
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-18 10:24:42 +0700

    modif: add method getUserDetail

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/data/repository/ApiRepository.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/data/repository/ApiRepository.kt
@@ -1,9 +1,10 @@
 package com.ardwiinoo.githubuserjetpack.data.repository
 
 import com.ardwiinoo.githubuserjetpack.data.remote.apiService.ApiService
+import com.ardwiinoo.githubuserjetpack.data.remote.response.GithubDetailResponse
 import com.ardwiinoo.githubuserjetpack.data.remote.response.GithubResponse
-import javax.inject.Inject
 import com.ardwiinoo.githubuserjetpack.utils.Result
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
@@ -14,6 +15,24 @@ class ApiRepository @Inject constructor(
     suspend fun searchUsers(username: String): Result<GithubResponse> {
 
         val response = apiService.getUsers(username)
+        val result = response.body()
+
+        return try {
+            if(response.isSuccessful && result != null) {
+                Result.Success(result)
+            } else {
+                val errorBody = response.errorBody()
+                val errorMessage = errorBody?.toString() ?: "Unknown error"
+                Result.Error(errorMessage)
+            }
+        } catch (e: Exception) {
+            Result.Error(e.message ?: "Unknown error")
+        }
+    }
+
+    suspend fun getDetailUser(username: String): Result<GithubDetailResponse> {
+
+        val response = apiService.getDetailUser(username)
         val result = response.body()
 
         return try {
