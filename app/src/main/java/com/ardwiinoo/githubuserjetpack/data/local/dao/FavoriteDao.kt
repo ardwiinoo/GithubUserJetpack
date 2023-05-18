b6e964d2b05974cacc9251a58a09b3adfc9edbd4
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-18 10:22:45 +0700

    modif: make dao for favorite table

--- app/src/main/java/com/ardwiinoo/githubuserjetpack/data/local/dao/FavoriteDao.kt
+++ app/src/main/java/com/ardwiinoo/githubuserjetpack/data/local/dao/FavoriteDao.kt
@@ -1,4 +1,22 @@
 package com.ardwiinoo.githubuserjetpack.data.local.dao
 
-class FavoriteDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ardwiinoo.githubuserjetpack.data.local.entity.Favorite
+
+@Dao
+interface FavoriteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(favorite: Favorite)
+
+    @Query("DELETE FROM favorite WHERE username = :username")
+    suspend fun deleteFavorite(username: String)
+
+    @Query("SELECT EXISTS(SELECT * FROM favorite WHERE username = :username)")
+    suspend fun isFavoriteUser(username: String): Boolean
+
+    @Query("SELECT * FROM favorite")
+    suspend fun getAllFavorites(): List<Favorite>
 }
